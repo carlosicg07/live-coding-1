58072e4935e6a67666af226159dff665f6fe4b4a
CIC <carlosicg07@gmail.com>
2023-04-27 15:32:40 -0600

    First commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí.
+addEventListener.
+let x= 'bocina';
+let y = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+bigwords(x,y);
+
+function bigwords(x,y){
+    y.forEach(z => {
+        if (z.length > x.length){
+            console.log(z);
+        }
+    });
+}
